ec5a4e05748e3901e7d0e2094ac48552476e247a
sky <1217268470@qq.com>
2023-01-06 22:06:39 +0800

    debug console

--- 51462/ver01/rules.mk
+++ 51462/ver01/rules.mk
@@ -6,8 +6,9 @@ BOOTLOADER = caterina
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
 COMMAND_ENABLE ?= yes
+CONSOLE_ENABLE ?= yes
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
+NKRO_ENABLE ?= yes
 TAP_DANCE_ENABLE = yes
